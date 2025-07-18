8a532e9dc45b1ff5f65726f47a91b8123bcabb29
GIGA MEDARD <medardgogo@gmail.com>
2025-07-18 00:08:16 +0000

    sending user gain from server to smart contract

--- app/Listeners/SessionFinishedEventListener.php
+++ app/Listeners/SessionFinishedEventListener.php
@@ -16,22 +16,13 @@ class SessionFinishedEventListener
     public function handle(SessionFinishedEvent $event): void
     {   
         try {
-            $user = $this->getUser($event->userId);
-            if (!$user || !$user->preMove) return;
+        $user = $this->getUser($event->userId);
+        if (!$user || !$user->preMove) return;
 
-            $fstPoolId = $user->preMove->session_first_pool_id;
-            Log::info("SessionFinishedEventListener: User ID: {$user->id}, first Pool ID: $fstPoolId");
+        $q = $this->calculateQValue($user);
+        Log::info("Calculated q: $q for User: {$user->id}");
 
-            [$fHistInitial, $fHistFinal] = $this->getSessionEdgesFHists($user->id, $fstPoolId);
-            if (!$fHistInitial || !$fHistFinal) return;
-
-            if ($fHistInitial->old_balance == 0) {
-                Log::warning("SessionFinishedEventListener: Initial balance is 0 for user ID: {$user->id}, Pool ID: $fstPoolId");
-                return;
-            }
-
-            $q = $this->calculateQValue($fHistInitial, $fHistFinal);
-            $this->processUserBalance($user, $q);
+        $this->processUserBalance($user, $q);
         } catch (\Exception $e) {
             Log::error("SessionFinishedEventListener: Exception occurred for user ID: {$event->userId} - " . $e->getMessage());
         }
@@ -65,7 +56,8 @@ class SessionFinishedEventListener
     }
 
     private function getSessionFHists($userId, $fstPoolId)
-    {
+    {   
+
         $fHistInitial = FHist::where(function ($query) use ($userId) {
                 $query->where('user1_id', $userId)
                       ->orWhere('user2_id', $userId);
@@ -73,37 +65,55 @@ class SessionFinishedEventListener
             ->where('pool_id', $fstPoolId)
             ->orderBy('pool_id', 'asc')
             ->first();
+        //Add null check for $fHistInitial
+        if (!$fHistInitial) {
+            Log::error("SessionFinishedEventListener: Initial FHist not found for User ID: $userId, Pool ID: $fstPoolId");
+            return [];
+        }
 
         $sessionfHist = FHist::where(function ($query) use ($userId) {
                 $query->where('user1_id', $userId)
                       ->orWhere('user2_id', $userId);
             })
-            ->where('id','>=', $fstfHistInitial->id)
+            ->where('id','>=', $fHistInitial->id)
             ->get();
 
-        return userSessionfHist;
+        return $sessionfHist;
     }
 
-    private function calculateQValue($fHistInitial, $fHistFinal): float
+    private function calculateQValue(User $user): float
     {
-        return $fHistFinal->balance / $fHistInitial->old_balance;
+        $initialTotal = $user->session_start_balance 
+                    + $user->session_start_battle_balance;
+        
+        $finalTotal = $user->balance 
+                    + $user->battle_balance;
+        
+        return $finalTotal / ($initialTotal ?: 1); // Avoid division by zero
     }
 
     private function processUserBalance(User $user, float $q): void
     {
-        if ($q >= 2) { 
+        if ($q >= config('game_settings.gain_coefficient')) { 
             $this->transferBattleBalance($user);
             //send sessionFHists to pinata
             $data = $this->getSessionFHists($user->id, $user->preMove->session_first_pool_id);
 
-            Log::info('SessionFinishedEventListener: Sending session FHists to Pinata: ' . json_encode($data));
-            
             $cid = Web3Helper::sendArchiveToPinata($data);
+            if (!$cid) {
+                Log::error("SessionFinishedEventListener: Failed to send session FHists to Pinata for User ID: {$user->id}");
+                return;
+            }
             //send cid to smart contract
             Web3Helper::sendSessionCIDToSmartContract(env('NODE_URL'), $cid, $user->wallet_address); 
+            //Web3Helper::sendSessionCIDToSmartContract(env('NODE_URL'), 'cid', $user->wallet_address); 
 
 
             $this->sendPayment($user);
+
+            Log::info('SessionFinishedEventListener: Sending session FHists to Pinata: ' . json_encode($data));
+            
+            
         } elseif ($q < 1 && $user->balance < $user->bet_amount) {
             // TODO: event(new UseAssurenceEvent($user->id));
             Log::info("SessionFinishedEventListener: User ID: {$user->id} triggered UseAssurenceEvent.");
@@ -117,6 +127,9 @@ class SessionFinishedEventListener
         $user->bet_amount = 0;
         $user->preMove->current_index = 0;
         $user->status = 'available';
+        $user->session_started = false; // Reset session_started after transfer
+        $user->session_start_balance = 0; // Reset session start balance
+        $user->session_start_battle_balance = 0; // Reset session start battle balance
         $user->save();
 
         Log::info("SessionFinishedEventListener: User ID: {$user->id} successfully transferred battle balance and reset status.");
